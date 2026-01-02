975aa3a4d12c7612c575c08b5a487bd94af1e624
Abhijit1326 <abhijitsonkamble01801@gmail.com>
2026-01-02 12:24:06 +0530

    Sum of two numbers

---
+++ cpp/sum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter number of array : ";
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << i << ": ";
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter the Target number : ";
+    cin >> target;
+
+    // The Logic
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
+
+                cout << "Index of target sum no. = " << " [ " << i << " , " << j << " ] " << endl;
+                cout << "Number of target sum no. = " << " [ " << arr[i] << " , " << arr[j] << " ] " << endl;
+
+                return 0;
+            }
+        }
+    }
+}
